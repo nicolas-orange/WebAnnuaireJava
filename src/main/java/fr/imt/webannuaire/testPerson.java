55f4524b5e3387d5622d6355beb8450a663ee5a4
Georges <nicolas.moreau@orange.com>
2022-10-28 12:13:51 +0200

    having the web server working in the main

--- src/main/java/fr/imt/webannuaire/testPerson.java
+++ src/main/java/fr/imt/webannuaire/testPerson.java
@@ -3,23 +3,35 @@
  */
 package fr.imt.webannuaire;
 
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 /**
  * @author INMO7350
  *
  */
+@SpringBootApplication
 public class testPerson {
 
 	/**
 	 * @param args
 	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+	    SpringApplication.run(testPerson.class, args);
+	    
 		Person totot = new Person ("dupont","toto","012345678","Paris");
 		Person totod = new Person("dupond","toto","012345679","Paris");
 		
 		System.out.println(totot);
 		System.out.println(totod);
+		
+		
 		
 	}
 
