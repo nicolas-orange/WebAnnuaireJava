9d2f3e78cc54aac6a170c4183a0df69718631ae1
Georges <nicolas.moreau@orange.com>
2022-10-28 11:50:47 +0200

    preparing the java

---
+++ src/main/java/fr/imt/webannuaire/data/Person.java
@@ -0,0 +1,78 @@
+package fr.imt.webannuaire.data;
+
+public class Person {
+	
+	private static int currentid=1;
+	private int id;
+	private String name;
+	private String surname;
+	private String phone;
+	private String city;
+
+	public Person() {
+	};
+
+	public Person(String pname, String psurname, String pphone, String pcity) {
+		super();
+		this.id = currentid;
+		currentid++;
+		this.name = pname;
+		this.surname = psurname;
+		this.phone = pphone;
+		this.city = pcity;
+	}
+	public Person(int pid, String pname, String psurname, String pphone, String pcity) {
+		this.id = pid;
+		this.name = pname;
+		this.surname = psurname;
+		this.phone = pphone;
+		this.city = pcity;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", surname=" + surname + ", phone=" + phone + ", city=" + city
+				+ "]";
+	}
+
+}
---
+++ src/main/java/fr/imt/webannuaire/data/testPerson.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package fr.imt.webannuaire.data;
+
+/**
+ * @author INMO7350
+ *
+ */
+public class testPerson {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person totot = new Person ("dupont","toto","012345678","Paris");
+		Person totod = new Person("dupond","toto","012345679","Paris");
+		
+		System.out.println(totot);
+		System.out.println(totod);
+		
+	}
+
+}
